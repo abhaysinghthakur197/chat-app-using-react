f6e0858411e74ffbbbf45d513e99c23c1b4811a8
abhay singh <abhaysinghthakur.982@gmail.com>
2020-12-05 09:09:06 +0530

    arrenged the structure of project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
 
 function App() {
   return (
